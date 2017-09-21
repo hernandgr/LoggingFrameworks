6f5ed7d9268a933e74a11859bd648351e3501fea
Hernan Guzman <hernandgr@gmail.com>
2017-09-20 20:17:56 -0500

    Added Raygun example

---
+++ RaygunDemo/RaygunDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace RaygunDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            throw new Exception("Oops!");
+        }
+    }
+}
