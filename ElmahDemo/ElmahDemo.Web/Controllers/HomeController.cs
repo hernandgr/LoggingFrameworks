77913f2e8b9f94efe07416f1a9e4d07d118120ef
Hernan Guzman <hernandgr@gmail.com>
2017-09-20 19:57:59 -0500

    Elmah demo added

---
+++ ElmahDemo/ElmahDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ElmahDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            throw new Exception("Oops!");
+        }
+    }
+}
